029b34336aab7d93d7cc2717a73a79747285b58d
dkasuga <benjamin.at.mac@gmail.com>
2020-08-08 11:56:50 +0900

    rewrite encoder in AE from torch to TF

--- modelAE.py
+++ modelAE.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow_addons as tfa
 
 import os
 import time
@@ -45,7 +46,7 @@ class generator(tf.keras.Model):
 
         self.leaky_relu = tf.keras.layers.LeakyReLU()
 
-    def __call__(self, points, z, is_training=False):
+    def __call__(self, points, z, training=False):
         zs = tf.broadcast_to(tf.reshape(
             z, [-1, 1, self.z_dim]), [1, points.shape[1], 1])
         pointz = tf.concat([points, zs], axis=2)
@@ -76,53 +77,50 @@ class generator(tf.keras.Model):
         return l7
 
 
-class encoder(nn.Module):
+class encoder(tf.keras.Model):
     def __init__(self, ef_dim, z_dim):
         super(encoder, self).__init__()
         self.ef_dim = ef_dim
         self.z_dim = z_dim
-        self.conv_1 = nn.Conv3d(
-            1, self.ef_dim, 4, stride=2, padding=1, bias=False)
-        self.in_1 = nn.InstanceNorm3d(self.ef_dim)
-        self.conv_2 = nn.Conv3d(
-            self.ef_dim, self.ef_dim*2, 4, stride=2, padding=1, bias=False)
-        self.in_2 = nn.InstanceNorm3d(self.ef_dim*2)
-        self.conv_3 = nn.Conv3d(
-            self.ef_dim*2, self.ef_dim*4, 4, stride=2, padding=1, bias=False)
-        self.in_3 = nn.InstanceNorm3d(self.ef_dim*4)
-        self.conv_4 = nn.Conv3d(
-            self.ef_dim*4, self.ef_dim*8, 4, stride=2, padding=1, bias=False)
-        self.in_4 = nn.InstanceNorm3d(self.ef_dim*8)
-        self.conv_5 = nn.Conv3d(self.ef_dim*8, self.z_dim,
+        self.conv_1 = tf.keras.layers.Conv3D(
+            self.ef_dim, 4, stride=2, padding='valid', use_bias=False, kernel_initializer=tf.initializers.GlorotUniform())
+        self.in_1 = tfa.layers.InstanceNormalization()
+        self.conv_2 = tf.keras.layers.Conv3D(
+            self.ef_dim * 2, 4, stride=2, padding='valid', use_bias=False, kernel_initializer=tf.initializers.GlorotUniform())
+        self.in_2 = tfa.layers.InstanceNormalization()
+        self.conv_3 = tf.keras.layers.Conv3D(
+            self.ef_dim * 4, 4, stride=2, padding='valid', use_bias=False, kernel_initializer=tf.initializers.GlorotUniform())
+        self.in_3 = tfa.layers.InstanceNormalization()
+        self.conv_4 = tf.keras.layers.Conv3D(
+            self.ef_dim * 8, 4, stride=2, padding='valid', use_bias=False, kernel_initializer=tf.initializers.GlorotUniform())
+        self.in_4 = tfa.layers.InstanceNormalization()
+        self.conv_5 = tf.keras.layers.Conv3D(
+            self.z_dim, 4, stride=2, padding='same', use_bias=True, kernel_initializer=tf.initializers.GlorotUniform(), bias_initializer='zeros')
+        self.conv_5 = nn.Conv3d(self.ef_dim * 8, self.z_dim,
                                 4, stride=1, padding=0, bias=True)
-        nn.init.xavier_uniform_(self.conv_1.weight)
-        nn.init.xavier_uniform_(self.conv_2.weight)
-        nn.init.xavier_uniform_(self.conv_3.weight)
-        nn.init.xavier_uniform_(self.conv_4.weight)
-        nn.init.xavier_uniform_(self.conv_5.weight)
-        nn.init.constant_(self.conv_5.bias, 0)
 
-    def forward(self, inputs, is_training=False):
-        d_1 = self.in_1(self.conv_1(inputs))
-        d_1 = F.leaky_relu(d_1, negative_slope=0.02, inplace=True)
+    def __call__(self, inputs, training=False):
+        d_1 = self.in_1(self.conv_1(inputs), training)
+        d_1 = leaky_relu(d_1, 0.02)
 
-        d_2 = self.in_2(self.conv_2(d_1))
-        d_2 = F.leaky_relu(d_2, negative_slope=0.02, inplace=True)
+        d_2 = self.in_2(self.conv_2(inputs), training)
+        d_2 = leaky_relu(d_2, 0.02)
 
-        d_3 = self.in_3(self.conv_3(d_2))
-        d_3 = F.leaky_relu(d_3, negative_slope=0.02, inplace=True)
+        d_3 = self.in_3(self.conv_3(inputs), training)
+        d_3 = leaky_relu(d_3, 0.02)
 
-        d_4 = self.in_4(self.conv_4(d_3))
-        d_4 = F.leaky_relu(d_4, negative_slope=0.02, inplace=True)
+        d_4 = self.in_4(self.conv_4(inputs), training)
+        d_4 = leaky_relu(d_4, 0.02)
 
         d_5 = self.conv_5(d_4)
-        d_5 = d_5.view(-1, self.z_dim)
-        d_5 = torch.sigmoid(d_5)
+        d_5 = tf.reshape(d_5, [-1, self.z_dim])
+
+        d_5 = tf.keras.activations.sigmoid(d_5)
 
         return d_5
 
 
-class im_network(nn.Module):
+class im_network(tf.kera.Model):
     def __init__(self, ef_dim, gf_dim, z_dim, point_dim):
         super(im_network, self).__init__()
         self.ef_dim = ef_dim
